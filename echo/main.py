405dc63355e5a04d33007b216212e0a43ccb54b1
kureLeGrill <vetok88@gmail.com>
2020-09-04 15:48:31 +0200

    add send message to same script

--- echo/main.py
+++ echo/main.py
@@ -4,10 +4,43 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler
 from telegram.ext import Filters
-import send_mail
+#import send_mail
+
+
 
 from echo.config import TG_TOKEN
 
+import os
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+def send_mails(mail_To):
+    EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+    EMAIL_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD')
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Завтрак 03.09.2020'
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = mail_To
+    msg.set_content('Прекрасного времени суток,\n' 
+                    'Вот ссылка на закрытый ютуб-канал:\n' 
+                    'https://youtu.be/DUaEfdcq_II\n'
+'Видео с завтрака 27.08.2020 на тему проработки лимитов и ограничений, с подробным описанием упражнений. Видео по ссылке являться эксклюзивным и не находиться в свободном доступе.\n' 
+'Хорошего настроения и приятного просмотра.\n' 
+'С уважением,\n'
+'Ганна\n')
+
+    #with open('','rb') as f:
+     #   file_data = f.read()
+      #  file_type = imghdr.what(f.name)
+      #  file_name = f.name
+
+    #msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+
+        smtp.send_message(msg)
 
 def do_start(bot: Bot, update: Update):
     bot.send_message(
@@ -23,7 +56,6 @@ def do_help(bot: Bot, update: Update):
         chat_id=update.message.chat_id,
         text="Privet! otpravj mne shto nibud",
     )
-    send_mail.send_mails()
 
 def do_echo(bot: Bot, update: Update):
     text = update.message.text
@@ -31,7 +63,7 @@ def do_echo(bot: Bot, update: Update):
         chat_id=update.message.chat_id,
         text="Mail send to "+text,
     )
-    send_mail.send_mails(text)
+    send_mails(text)
 
 
 def main():
